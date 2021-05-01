94bf5c921497bd937055266beaa69cda435ec278
Michael Kreher <mk1977-dd@web.de>
2021-05-01 10:41:36 +0200

    nRF5 SDK 17.0.2 added.

---
+++ barcode_scanner_device/src/ble_services/ble_hid_barcode.h
@@ -0,0 +1,10 @@
+#ifndef BLE_HID_BARCODE_H__
+#define BLE_HID_BARCODE_H__
+
+#include "ble.h"
+#include "ble_srv_common.h"
+#include "app_util_platform.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif // BLE_HID_BARCODE_H__
